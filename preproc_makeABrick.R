c47b24ccf9a0ae3f6e3097d4b18c2394a2b8d956
Tim Howard <tghoward@gmail.com>
2016-06-08 14:24:53 -0400

    make a brick

---
+++ preproc_makeABrick.R
@@ -0,0 +1,30 @@
+#####
+# This script can make a raster brick out of a folder of tiff files.
+# A brick might speed up processing a little bit over a raster stack
+# but also may create an unwieldy monstrosity.
+#
+# Modified from a script written by Emilie Henderson
+####
+
+library(raster)
+
+# set this path to the folder where the environmental rasters reside
+# it can have other files, but only *.tifs that you want to be included
+# in the brick
+pathToTifs <- "G:/SDM_test/env_rasters"
+
+# get a list of the grids
+tiflist <- list.files(path = pathToTifs, pattern = ".tif$")
+
+# tack on the full paths and name them
+gridlist<-as.list(paste(pathToTifs,tiflist,sep = "/"))
+nm <- substr(tiflist,1,nchar(tiflist) - 4)
+names(gridlist)<-nm
+
+# make the brick, writing it out at the same time
+envBrick <- brick(stack(gridlist),filename = paste(pathToTifs,"/brick.grd",sep = ""))
+  
+# ## To look at the brick, use the following lines:
+# plot(envBrick) #plots all the layers in the brick
+# plot(envBrick,nm[1]) #plots only layer 1, for example
+
