7520f7875f75d42a040c248dc7247ec3faee541c
Tim Howard <tghoward@gmail.com>
2016-06-22 08:11:47 -0400

    finish refactoring

--- 1_randomPointsInPolys.R
+++ 1_randomPointsInPolys.R
@@ -14,6 +14,7 @@ library(rgdal)
 #   e.g. glypmuhl.shp
 # - the polygon shapefile has these fields EO_ID, SCIEN_NAME, COMMONNAME, ERACCURACY
 
+#######
 ######
 ## these are the lines you need to change
 
@@ -32,44 +33,46 @@ db<-dbConnect(SQLite(),dbname=db_file)
 
 ## should not need to change anything else below here
 #######
+#######
 
 #get a list of what's in the directory
 fileList <- dir( pattern = ".shp$")
 
 #loop through all species (having only one is ok)
 for (fileName in fileList){
-  shpName <- strsplit(fileName,"\\.")[[1]][[1]]
-  sppCode <- shpName
+	shpName <- strsplit(fileName,"\\.")[[1]][[1]]
+	sppCode <- shpName
 
 	shapef <- readOGR(fileName, layer = shpName)
 	#explode multi-part polys
 	shp_expl <- disaggregate(shapef)
 	#strip some columns
 	colList <- c(grep("^EO_ID$",names(shp_expl@data)),
-		   grep("SCIEN_NAME",names(shp_expl@data)),
-		   grep("COMMONNAME",names(shp_expl@data)),
-			   grep("ERACCURACY",names(shp_expl@data)))
+		grep("SCIEN_NAME",names(shp_expl@data)),
+		grep("COMMONNAME",names(shp_expl@data)),
+		grep("ERACCURACY",names(shp_expl@data)))
 	shp_expl@data <- shp_expl@data[,colList]
 
 	#add some columns (explode id and area)
 	shp_expl@data <- cbind(shp_expl@data, 
-			EXPL_ID = rownames(shp_expl@data), 
-			AREAM2 = sapply(slot(shp_expl, "polygons"), slot, "area"))
+		EXPL_ID = rownames(shp_expl@data), 
+		AREAM2 = sapply(slot(shp_expl, "polygons"), slot, "area"))
 			
 	#write out the exploded polygon set
 	nm.PyFile <- paste(sppCode, "_expl", sep = "")
 	writeOGR(shp_expl, dsn = outdir, layer = nm.PyFile, driver="ESRI Shapefile", overwrite_layer=TRUE)
 	  
-    #name of output shapefile for random points within polygons
+    #name of random points output shapefile; add path to (now input) polygon file
     nm.RanPtFile <- paste(outdir,"/", sppCode, "_RanPts", sep = "")
-
+	nm.PyFile <- paste(outdir,"/", sppCode, "_expl", sep = "")
+	
 	#tell the console what's up
 	print(paste("Beginning on ", 
 		sppCode, ", ", grep(fileName, fileList) , " of ", length(fileList), sep = ""))
 
-	# send all the coming GRTS info messages to a file, so it's easier to 
-	# track progress on the terminal. This is optional.
-	# sinkName <- paste("RunMsgs_",Sys.Date(), ".txt",sep="")
+	# Optional: send all the coming GRTS info messages to a file, so it's easier to 
+	# track progress on the terminal.
+	# sinkName <- paste(outdir, "/", "RunMsgs_",Sys.Date(), ".txt",sep="")
 	# sink(file = sinkName, append=TRUE, type="output")
 
 	###############################
@@ -77,108 +80,97 @@ for (fileName in fileList){
 	#####
 	###############################
       
-      #get the attribute table from above 
-	  att.pt <- shp_expl@data
-	  
+	#get the attribute table from above 
+	att.pt <- shp_expl@data
+
 	# just in case convert to upper
 	names(att.pt) <- toupper(names(att.pt))
 	
-      #add another copy of the expl_ID field - the original becomes 'mdcaty' in 
-      #the final output
-      att.pt$EXPL_ID2 <- att.pt$EXPL_ID
+	#add another copy of the expl_ID field - the original becomes 'mdcaty' in 
+	#the final output
+	att.pt$EXPL_ID2 <- att.pt$EXPL_ID
 
-      #calculate Number of points for each poly, stick into new field
-      att.pt$PolySampNum <- round(400*((2/(1+exp(-(att.pt[,"AREAM2"]/900+1)*0.004)))-1))
-      #make a new field for the design, providing a stratum name
-      att.pt <- cbind(att.pt, "panelNum" = paste("poly_",att.pt$EXPL_ID, sep=""))
+	#calculate Number of points for each poly, stick into new field
+	att.pt$PolySampNum <- round(400*((2/(1+exp(-(att.pt[,"AREAM2"]/900+1)*0.004)))-1))
+	#make a new field for the design, providing a stratum name
+	att.pt <- cbind(att.pt, "panelNum" = paste("poly_",att.pt$EXPL_ID, sep=""))
 
-      #create the vector for indicating how many points to put in each polygon, 
-      #then each value in the vector needs to be attributed to the sampling unit 
-      #(either EO_ID or Shape_ID)
-      sampNums <- c(att.pt[,"PolySampNum"])
-      names(sampNums) <- att.pt[,"EXPL_ID"]
-      # sample MUST be larger than 1 for any single polygon use OVER to increase 
-      # sample sizes in these. To handle this, create a vector that contains 
-      # 2 when sample size = 1, otherwise 0
-      overAmt <- ifelse(sampNums == 1,2,0)
+	#create the vector for indicating how many points to put in each polygon, 
+	#then each value in the vector needs to be attributed to the sampling unit 
+	#(either EO_ID or Shape_ID)
+	sampNums <- c(att.pt[,"PolySampNum"])
+	names(sampNums) <- att.pt[,"EXPL_ID"]
+	# sample MUST be larger than 1 for any single polygon use OVER to increase 
+	# sample sizes in these. To handle this, create a vector that contains 
+	# 2 when sample size = 1, otherwise 0
+	overAmt <- ifelse(sampNums == 1,2,0)
 
-      #initialize the design list and the names vector so 
-      #they are available in the for loop
-      SampDesign <- vector("list",length(sampNums))
-      namesVec <- vector("list", length(sampNums))
+	#initialize the design list and the names vector so 
+	#they are available in the for loop
+	SampDesign <- vector("list",length(sampNums))
+	namesVec <- vector("list", length(sampNums))
 
-      # whew!  build the sampling design, as required by GRTS
-      # this is a list 'SampDesign' with internal lists: 'panel', 'seltype', and 
-      # 'over' for each entry of SampDesign
-      for (i in 1:length(sampNums)){
-        #build a vector of names to apply after the for loop
-        namesVec[i] <- paste("poly_",i,sep="")
-        #initialize the internal list
-        SampDesign[[i]] <- vector("list",3)
-        #populate the internal list
-        SampDesign[[i]] <- list(panel=c(FirstSamp=sampNums[[i]]),
-                                seltype="Equal", 
-                                over=overAmt[[i]])
-      }
+	# Build the sampling design, as required by GRTS
+	# this is a list 'SampDesign' with internal lists: 'panel', 'seltype', and 
+	# 'over' for each entry of SampDesign
+	for (i in 1:length(sampNums)){
+		#build a vector of names to apply after the for loop
+		namesVec[i] <- paste("poly_",i,sep="")
+		#initialize the internal list
+		SampDesign[[i]] <- vector("list",3)
+		#populate the internal list
+		SampDesign[[i]] <- list(panel=c(FirstSamp=sampNums[[i]]),
+				seltype="Equal", 
+				over=overAmt[[i]])
+	}
+	#apply that names vector
+	names(SampDesign) <- namesVec
 
-      #apply that names vector
-      names(SampDesign) <- namesVec
+	# if you want to check out this design, 
+	# a couple of exploratory commands here, commented out
+	# list the names of one of the sub lists
+	#names(SampDesign[[1]])
+	# show something about the structure of the list
+	#summary(SampDesign)  ##str(SampDesign) is even more thorough
 
-      #if you want to check out this design, 
-      #a couple of exploratory commands here, commented out
-        #list the names of one of the sub lists
-      #names(SampDesign[[1]])
-        #show something about the structure of the list
-      #summary(SampDesign)  ##str(SampDesign) is even more thorough
+	# Create the GRTS survey design
+	Unequalsites <- grts(design=SampDesign,
+				 src.frame="shapefile", #source of the frame
+				 in.shape=nm.PyFile,    #name of input shapefile no extension
+				 att.frame=att.pt,      #attributes associated with elements in the frame
+				 type.frame="area",     #type of frame:"finite", "linear", "area"
+				 stratum="panelNum",	#stratum field in att.pt
+				 mdcaty="EXPL_ID",		#categories for random pt probabilities
+				 DesignID= sppCode,  	#name for the design, used to create a site ID
+				 shapefile=TRUE,
+				 prj=nm.PyFile,
+				 out.shape=nm.RanPtFile)
 
-      # Create the GRTS survey design
-      Unequalsites <- grts(design=SampDesign,
-                     src.frame="shapefile", #source of the frame
-                     in.shape=nm.PyFile,    #name of input shapefile no extension
-                     att.frame=att.pt,      #a data frame of attributes associated with elements in the frame
-                     type.frame="area",     #type of frame:"finite", "linear", "area"
-                     stratum="panelNum",
-                     mdcaty="EXPL_ID",
-                     DesignID= sppCode,  #name for the design, which is used to create a site ID for each site.
-                     shapefile=TRUE,
-                     prj=nm.PyFile,
-                     out.shape=nm.RanPtFile)
+	###############################
+	#####     Write out various stats and data to the database
+	#####
+	###############################
 
-## GRTS just wrote the shapefile here. So now would be the time to open it up again and delete columns
-	  
-	  ###############################
-      #####     Write out various stats and data to the database
-      #####
-      ###############################
+	# prep the data
+	OutPut <- data.frame(SciName = paste(att.pt[1,"SCIEN_NAME"]),
+		CommName=paste(att.pt[1,"COMMONNAME"]),
+		ElemCode=sppCode,
+		RandomPtFile=nm.RanPtFile,
+		date = paste(Sys.Date()),
+		time = format(Sys.time(), "%X"),
+		Loc_Use=""
+		)
 
-      # prep the data
-      OutPut <- data.frame(SciName = paste(att.pt[1,"SCI_NAME"]),
-                   CommName=paste(att.pt[1,"COMMNAME"]),
-                   ElemCode=sppCode,
-                   RandomPtFile=nm.RanPtFile,
-                   date = paste(Sys.Date()),
-                   time = format(Sys.time(), "%X"),
-                   Loc_Use=""
-                   )
+	#Write the data to the SQLite database
+	dbWriteTable(db,"tblPrepStats",OutPut,append=TRUE)
+	###############################
 
-      #write the data to the database
-      #sqlSave(Cn.MDB.out, OutPut, tablename = "tblPrepStats", 
-      #                            append = TRUE, 
-      #                            rownames = FALSE)
-
-      #Write the data to the SQLite database
-      dbWriteTable(db,"tblPrepStats",OutPut,append=TRUE)
-      ###############################
-      
-      #stop the sink
-      # sink()
-      #tell the console what's up
-      print(paste("Finished with ", sppCode, sep=""))
+	#stop the sink
+	# sink()
+	#tell the console what's up
+	print(paste("Finished with ", sppCode, sep=""))
 #close the for loop
 }
-
-# close the connection to the Access DB
-#close(Cn.MDB.out)
 
 #Close Connection to the SQL DB
 dbDisconnect(db)
