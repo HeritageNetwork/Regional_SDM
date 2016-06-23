d1ee95f98183dc139ba7ba45e3b48ed9064da104
Tim Howard <tghoward@gmail.com>
2016-06-23 09:02:45 -0400

    background points script

---
+++ preproc_makeBackgroundPoints.R
@@ -0,0 +1,40 @@
+# File: preproc_makeBackgroundPoints.r
+# Purpose: GRTS sampling of study area polygon to generate background random points
+
+library(spsurvey)
+library(sp)
+library(rgdal)
+
+# This is the directory that has your study area polygon.
+setwd("G:/SDM_test/")
+
+# the name of the study area polygon
+fileName <- "testArea.shp"
+
+# read in the shapefile, get the attribute data
+layer <- strsplit(fileName,"\\.")[[1]][[1]]
+shapef <- readOGR(fileName, layer = layer)
+att.pt <- shapef@data
+
+# name of random points output shapefile
+nm.RanPtFile <- paste(layer, "_RanPts", sep = "")
+
+# Enter the number of random points you want to generate 
+numpts <- 1000
+
+# Create the design list
+dsgn <- list(None=list(panel=c(Panel=numpts), seltype="Equal"))
+
+# Call the grts function
+sites <- grts(design=dsgn,
+			src.frame="shapefile",
+			in.shape=layer,
+			att.frame=att.pt,
+			type.frame="area",
+			DesignID="bkgrndRanPts",
+			shapefile=TRUE,
+			prjfilename=layer,
+			out.shape=nm.RanPtFile)
+			
+			
+# the random points shapefile should now be written to the working directory			
