cf45b95c5a85bddcec68859a8fe2c96a8e28d309
Tim Howard <tghoward@gmail.com>
2016-08-25 08:26:14 -0400

    clean up

--- preproc_writeNativeRrasters.R
+++ preproc_writeNativeRrasters.R
@@ -3,22 +3,18 @@
 # write them out in native R raster format
 #
 ####
-### Kirsten is playing around on GitHub
 
 library(raster)
 
 # set this path to the folder where the environmental rasters reside
-# it can have other files, but only *.tifs that you want to be included
-# in the brick
+# it can have other files, but only *.tifs that you want to write to the R format
+
 pathToTifs <- "G:/RegionalSDM/env_vars/geotiffs"
 
 setwd(pathToTifs)
 
-# Joe was here.
-
 # set the output path
 outPath <- "G:/RegionalSDM/env_vars/nativeR"
-
 
 # get a list of the grids
 tiflist <- list.files(pattern = ".tif$")
@@ -28,7 +24,6 @@ for(tif in tiflist){
 	x <- raster(tif)
 	nm <- substr(tif,1,nchar(tif) - 4)
 	writeRaster(x, filename = paste(outPath,"/",nm,sep=""), format = "raster")
-	
 	}
 
 
