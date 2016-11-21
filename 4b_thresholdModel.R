850e8de4f3ade4aa5229db9b3cd313789f4225b4
Tim Howard <tghoward@gmail.com>
2016-11-21 08:12:46 -0500

    add sens and spec metrics

--- 4b_thresholdModel.R
+++ 4b_thresholdModel.R
@@ -4,6 +4,7 @@
 ## start with a fresh workspace with no objects loaded
 library(raster)
 library(rgdal)
+library(ROCR)
 
 inPath <- "G:/RegionalSDM/outputs"
 gridpath <- "G:/RegionalSDM/outputs/grids"
@@ -27,15 +28,26 @@ MTP <- min(y$X1)
 #get 10 percentile training presence
 TenPctile <- quantile(y$X1, prob = c(0.1))
 
-#equal sensitivity and specificity
+#max sensitivity plus specificity (maxSSS per Liu et al 2016)
+rf.full.sens <- performance(rf.full.pred,"sens")
+rf.full.spec <- performance(rf.full.pred,"spec")
+rf.full.sss <- data.frame(cutSens = unlist(rf.full.sens@x.values),sens = unlist(rf.full.sens@y.values),
+                          cutSpec = unlist(rf.full.spec@x.values), spec = unlist(rf.full.spec@y.values))
 
-#max sensitivity plus specificity
+rf.full.sss$sss <- with(rf.full.sss, sens + spec)
+maxSSS <- rf.full.sss[which.max(rf.full.sss$sss),"cutSens"]
+
+#equal sensitivity and specificity
+rf.full.sss$diff <- abs(rf.full.sss$sens - rf.full.sss$spec)
+eqss <- rf.full.sss[which.min(rf.full.sss$diff),"cutSens"]
 
 
 # load the prediction grid
 ras <- raster(paste(gridpath,"/",ElementNames$Code, ".tif", sep = ""))
 #set a threshhold. This is from...
 threshold <- round(rf.full.ctoff[2],3)
+threshold <- round(maxSSS, 4)
+
 # reclassify the raster based on the threshold
 rasrc <- reclassify(ras, c(-Inf,threshold,0,  threshold,Inf,1))
 
