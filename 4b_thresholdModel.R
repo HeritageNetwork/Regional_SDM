a88b6592fd94b171c9352b85e42305b1d070a6f7
Tim Howard <tghoward@gmail.com>
2016-12-12 15:36:01 -0500

    remove cutoff calcs from script#3, write threshold data to database in 4b

--- 4b_thresholdModel.R
+++ 4b_thresholdModel.R
@@ -21,13 +21,18 @@ fileName <- d[[n]]
 load(paste(inPath,fileName, sep="/"))
 
 ## Calculate different thresholds ----
+#set an empty list
+cutList <- list()
+
 #get minimum training presence
 x <- data.frame(rf.full$y, rf.full$votes)
 y <- x[x$rf.full.y ==1,]
 MTP <- min(y$X1)
+cutList$MTP <- list("value" = MTP, "code" = "MTP")
 
 #get 10 percentile training presence
 TenPctile <- quantile(y$X1, prob = c(0.1))
+cutList$TenPctile <- list("value" = TenPctile, "code" = "TenPctile")
 
 #max sensitivity plus specificity (maxSSS per Liu et al 2016)
 rf.full.sens <- performance(rf.full.pred,"sens")
@@ -37,15 +42,61 @@ rf.full.sss <- data.frame(cutSens = unlist(rf.full.sens@x.values),sens = unlist(
 
 rf.full.sss$sss <- with(rf.full.sss, sens + spec)
 maxSSS <- rf.full.sss[which.max(rf.full.sss$sss),"cutSens"]
+cutList$maxSSS <- list("value" = maxSSS, "code" = "maxSSS")
 
 #equal sensitivity and specificity
 rf.full.sss$diff <- abs(rf.full.sss$sens - rf.full.sss$spec)
 eqss <- rf.full.sss[which.min(rf.full.sss$diff),"cutSens"]
+cutList$eqss <- list("value" = eqss, "code" = "eqSS")
 
-allThresh <- data.frame("MTP" = MTP, "tenthPctile" = TenPctile, 
-                        "maxSumSensSpec" = maxSSS, "eqSensSpec" = eqss, 
-                        "FMeasure" = rf.full.ctoff[2], "FMeasAlpha" = alph,
-                        row.names = ElementNames$Code)
+
+# F-measure cutoff skewed towards capture more presence points
+# extract the precision-recall F-measure from training data
+# set alpha very low to tip in favor of 'presence' data over 'absence' data
+# based on quick assessment in Spring 07, set alpha to 0.01
+alph <- 0.01
+#create the prediction object for ROCR. Get pres col from votes (=named "1")
+rf.full.pred <- prediction(rf.full$votes[,"1"],df.full$pres)
+#use ROCR performance to get the f measure
+rf.full.f <- performance(rf.full.pred,"f",alpha = alph)
+#extract the data out of the S4 object, then find the cutoff that maximize the F-value.
+rf.full.f.df <- data.frame(cutoff = unlist(rf.full.f@x.values),fmeasure = unlist(rf.full.f@y.values))
+rf.full.ctoff <- c(1-rf.full.f.df[which.max(rf.full.f.df$fmeasure),][["cutoff"]], rf.full.f.df[which.max(rf.full.f.df$fmeasure),][["cutoff"]])
+#rf.full.ctoff <- c(1-rf.full.f.df[which.max(rf.full.f.df$fmeasure),][[1]], rf.full.f.df[which.max(rf.full.f.df$fmeasure),][[1]])
+names(rf.full.ctoff) <- c("0","1")
+FMeasPt01 <- rf.full.ctoff[2]
+cutList$FMeasPt01 <- list("value" = FMeasPt01, "code" = "FMeasPt01")
+
+# collate and write to DB ----
+
+# number of thresholds to write to the db
+numThresh <- length(cutList)
+
+allThresh <- data.frame("ElemCode" = rep(ElementNames$Code, numThresh),
+                "dateTime" = rep(as.character(Sys.time()), numThresh),
+                "cutCode" = unlist(lapply(cutList, function(x) x[2])),
+                "cutValue" = unlist(lapply(cutList, function(x) x[1])),
+                stringsAsFactors = FALSE)
+
+db <- dbConnect(SQLite(),dbname=db_file)
+
+
+op <- options("useFancyQuotes")
+options(useFancyQuotes = FALSE)
+
+for(i in 1:numThresh){
+  SQLquery <- paste("INSERT INTO tblCutoffs (", 
+                    toString(names(allThresh)),
+                    ") VALUES (",
+                    toString(sQuote(allThresh[i,])),
+                    ");", sep = "")
+  dbExecute(db, SQLquery)
+}
+
+# clean up
+options(op)
+dbDisconnect(db)
+
 
 ## choose threshold, create binary grid ----
 #lets set the threshold to MTP
