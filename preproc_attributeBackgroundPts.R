0fe56e6243e84bc5507dc3c02c88b347fec90b36
Tim Howard <tghoward@gmail.com>
2016-12-12 10:09:17 -0500

    remove all objects at end of each script. Some cleanup.

--- preproc_attributeBackgroundPts.R
+++ preproc_attributeBackgroundPts.R
@@ -5,32 +5,33 @@
 library(raster)
 library(rgdal)
 
+## set paths ----
 pathToRas <- "D:/RegionalSDM/env_vars/geotiffs"
 pathToPts <- "D:/RegionalSDM/inputs/background"
 
+## create a stack ----
 setwd(pathToRas)
-## create a stack. Note this is using native R rasters
 raslist <- list.files(pattern = ".tif$")
 gridlist <- as.list(paste(pathToRas,raslist,sep = "/"))
 nm <- substr(raslist,1,nchar(raslist) - 4)
 names(gridlist) <- nm
 envStack <- stack(gridlist)
 
-## Set working directory to the random points location
+## Get random points file ----
 setwd(pathToPts)
-
 ranPtsFile <- "clpBnd_SDM_RanPts_clean.shp"
 ranPtsFileNoExt <- sub(".shp","",ranPtsFile)
-
-##Read these files into a list of SpatialPoints dataframes
+# Read these files into a list of SpatialPoints dataframes
 shpf <- readOGR(".", layer = ranPtsFileNoExt)
   
-#Get a list of the codes (this assumes all the input files had '_RanPts.shp' that shall be stripped)
+# Get a list of the codes (this assumes all the input files had '_RanPts.shp' that shall be stripped)
 code_name <- substr(ranPtsFile,1,(nchar(ranPtsFile)-11))
 
-# do it, write it
+# do it, write it ----
 x <- extract(envStack, shpf, method="simple", sp=TRUE)
 filename <- paste(code_name, "_att", sep="")
 writeOGR(x, pathToPts, layer=paste(filename), driver="ESRI Shapefile", overwrite_layer=TRUE)
 
-
+## clean up ----
+# remove all objects before using another script
+rm(list=ls())
