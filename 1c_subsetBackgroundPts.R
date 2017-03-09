63863ecdb81e3975214ce117ed7fc6a2b24d8bdb
Tim Howard <tghoward@gmail.com>
2017-03-09 12:19:49 -0500

    include subset code

--- 1c_subsetBackgroundPts.R
+++ 1c_subsetBackgroundPts.R
@@ -1,4 +1,4 @@
-# File: 1b_removeCoincidentBackgroundPts.r
+# File: 1c_
 # Purpose: remove random background points that are within polygons (and a 30 m buffer)
 # of known locations (EO polygons)
 
@@ -27,24 +27,30 @@ backgShapef <- readOGR(dsn=ranptsFolder, layer=ranptsShp)
 #get projection info for later
 projInfo <- backgShapef@proj4string
 
-# find coincident points ----
-#buffer the poly shapefile 30 m
-polybuff <- gBuffer(polyShapef, width = 30)
+# random subset ----
+# how many background points do we want?
+desiredBG <- 1000
 
-# find points that fall within the buffered polygons, subset the sp object
-coincidentPts <- gContains(polybuff, backgShapef, byid = TRUE)
-colnames(coincidentPts) <- "insideBuff"
-backgShapef@data <- cbind(backgShapef@data, coincidentPts)
-backgSubset <- backgShapef[backgShapef@data$insideBuff == FALSE,]
+# these points are already spatially-balanced, per GRTS
+# so to maintain this, we actually draw *in order*, based 
+# on the siteID column
+# Data are already sorted but to be absolutely sure, sort 'em
+backgShapef <- backgShapef[order(backgShapef$siteID),]
+
+# get the subset
+BG.subset <- backgShapef[1:desiredBG,]
 
 # projection info doesn't stick, apply from what we grabbed earlier
-backgSubset@proj4string <- projInfo
+BG.subset@proj4string <- projInfo
 
 # write it out ---
-outFileName <- paste(ranptsShp, "_clean", sep="")
-writeOGR(backgSubset, dsn = ranptsFolder, layer = outFileName, 
+outFileName <- paste(ranptsShp, "_subset", sep="")
+writeOGR(BG.subset, dsn = ranptsFolder, layer = outFileName, 
          driver="ESRI Shapefile", overwrite_layer=TRUE)
 
 ## clean up ----
 # remove all objects before moving on to the next script
 rm(list=ls())
+
+
+
