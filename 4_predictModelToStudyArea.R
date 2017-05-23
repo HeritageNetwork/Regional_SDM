0a946271571606514d4c8915b27076c8a101ed74
Christopher Tracey <ctracey@paconserve.org>
2017-05-23 14:48:56 -0400

    completion of the predict

--- 4_predictModelToStudyArea.R
+++ 4_predictModelToStudyArea.R
@@ -3,8 +3,10 @@
 
 ## start with a fresh workspace with no objects loaded
 #library(raster) # do we stil need this - CT
+library(raster)
 library(rgdal)
 library(randomForest)
+library(data.table)
 
 ####
 ## two lines need your attention. The one directly below (loc_scripts)
@@ -15,7 +17,7 @@ loc_scripts <- "E:/SDM/Aquatic/scripts/Regional_SDM"
 # get paths, other settings
 source(paste(loc_scripts,"0_pathsAndSettings.R", sep="/"))
 # get the customized version of the predict function
-source(paste(loc_scripts, "RasterPredictMod.R", sep = "/"))
+#source(paste(loc_scripts, "RasterPredictMod.R", sep = "/"))
 
 # load data ----
 # get the rdata file
@@ -30,25 +32,34 @@ load(fileList[[n]])
 setwd(loc_envVars)
 EnvVars <- read.csv("EnvVars.csv", colClasses=c("HUC12"="character"))
 names(EnvVars) <- tolower(names(EnvVars))
+EnvVars$huc12 <- NULL
 
-#Make the raster stack
-##stackOrder <- names(df.full)[indVarCols]
-##setwd(loc_envVars)
-##rasL <- paste(stackOrder,".tif", sep="")
-##fullL <- as.list(paste(loc_envVars, rasL, sep="/"))
-##names(fullL) <- stackOrder
-##envStack <- stack(fullL)
+# need to understand what's going on here a little more - CT
+# run prediction ----
 
-## load the reach shapefile for the study area
+df.all <- df.full
+
+result <- predict(rf.full, df.all[,indVarCols], type="prob")
+result <- result[,-1]
+df.all[ ,(ncol(df.all)+1)] <- result
+setnames(df.all, "V34", "probability")
+results_join_table <- df.all[,c(5,34)]
+
+#fileNm <- paste(loc_outRas, "/", ElementNames$Code, "_",Sys.Date(),".tif", sep = "")
+#outRas <- predictRF(EnvVars, rf.full, progress="text", index=2, na.rm=TRUE, type="prob", filename=fileNm, format = "GTiff", overwrite=TRUE)
+
+# load the reach shapefile for the study area
 setwd(loc_otherSpatial)
 StudyAreaReaches <- "flowlines.shp" # the name of the study area flowlines
 layer <- strsplit(StudyAreaReaches,"\\.")[[1]][[1]]
 shapef <- readOGR(StudyAreaReaches, layer = layer)
 
-# need to understand what's going on here a little more - CT
-# run prediction ----
-fileNm <- paste(loc_outRas, "/", ElementNames$Code, "_",Sys.Date(),".tif", sep = "")
-outRas <- predictRF(EnvVars, rf.full, progress="text", index=2, na.rm=TRUE, type="prob", filename=fileNm, format = "GTiff", overwrite=TRUE)
+# join probability to shapefile -- https://stackoverflow.com/questions/5732064/merge-data-vector-to-shapefile-data-slot
+shapef@data <- data.frame(shapef@data, results_join_table[match(shapef@data$COMID, results_join_table$comid),])
+
+
+# write the shapefile
+writeOGR(obj=shapef, dsn="E:/SDM/Aquatic/outputs/shapefiles", layer="lasmcomp_results", driver="ESRI Shapefile")
 
 ## clean up ----
 # remove all objects before moving on to the next script
