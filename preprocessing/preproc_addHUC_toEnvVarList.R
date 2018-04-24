e3dd87d005b8c3ebd49cd6be82f384450c0319de
Christopher Tracey <ctracey@paconserve.org>
2018-04-24 13:59:46 -0400

    got rid of rownames

--- preprocessing/preproc_addHUC_toEnvVarList.R
+++ preprocessing/preproc_addHUC_toEnvVarList.R
@@ -6,7 +6,7 @@
 ###
 ## two lines need your attention. The one directly below (loc_scripts)
 ## and about line 43 where you choose which random points file to use
-loc_scripts <- "E:/SDM/Aquatic/scripts/Regional_SDM"
+loc_scripts <- "E:/SDM/Aquatic2/scripts/Regional_SDM"
 
 source(paste(loc_scripts, "0_pathsAndSettings.R", sep = "/"))
 setwd(loc_envVars)
@@ -15,17 +15,14 @@ EnvVars <- read.csv("EnvVars.csv", colClasses=c("huc12"="character"))
 # fix the number of digits in the huc12 if the leading zeros are stripped.
 library(stringr)
 EnvVars$huc12 <- str_pad(EnvVars$huc12,12,pad="0")
-
 # enter the huc level you want to add (eg. 2,4,6,8,10,12) 
 hucLevel <- 4
 EnvVars$hucSubset <- substr(EnvVars$huc12,1,hucLevel)
-
 # replace the old EnvVars.csv with the new one
-file.rename(from="E:/SDM/Aquatic/env_vars/EnvVars.csv",to="E:/SDM/Aquatic/env_vars/old/EnvVars1.csv")
+file.rename(from="E:/SDM/Aquatic2/env_vars/EnvVars.csv",to="E:/SDM/Aquatic2/env_vars/old/EnvVars1.csv")
 # write the new file as a csv
-write.csv(EnvVars_added, "EnvVars.csv")
-
-# note, you should always rerun and check the correlations after adding variables.
+write.csv(EnvVars, "EnvVars.csv", row.names=FALSE)
+# note: you should always rerun and check the correlations after adding variables.
 
 # insert row into database with variable name
 db <- dbConnect(SQLite(),dbname=nm_db_file)
