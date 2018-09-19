d5a0229df5f6bb6b72c597f87c8765f2eb940902
David Bucklin <david.bucklin@dcr.virginia.gov>
2018-09-19 10:39:01 -0400

    major changes to run_SDM. New simplified folder structure, archived input datasets, DB updates

--- 1_pointsInPolys_cleanBkgPts.R
+++ 1_pointsInPolys_cleanBkgPts.R
@@ -25,30 +25,40 @@ library(raster)
 ## two lines need your attention. The one directly below (loc_scripts)
 ## and about line 38 where you choose which polygon file to use
 
-#loc_scripts <- "K:/Reg5Modeling_Project/scripts/Regional_SDM"
-#source(paste(loc_scripts, "0_pathsAndSettings.R", sep = "/"))
-
-setwd(loc_spPoly)
+# load data, QC ----
+setwd(loc_model)
+dir.create(paste0(model_species,"/inputs/presence"), recursive = T, showWarnings = F)
+dir.create(paste0(model_species,"/inputs/model_input"), showWarnings = F)
+setwd(paste0(loc_model,"/",model_species,"/inputs/presence"))
+# changing to this WD temporarily allows for presence file to be either in presence folder or specified with full path name
 
 # load data, QC ----
-fileName <- nm_spPoly
-shpName <- strsplit(fileName,"\\.")[[1]][[1]]
-sppCode <- shpName
-
-presPolys <- readOGR(dsn=loc_spPoly, layer = shpName) #Z-dimension discarded msg is OK
+fileName <- basename(nm_presFile)
+presPolys <- readOGR(dsn=dirname(nm_presFile), layer = gsub(".shp$", "", fileName)) #Z-dimension discarded msg is OK
 #check for proper column names. If no error from next code block, then good to go
-presPolys$RA <- presPolys$SFRACalc
 shpColNms <- names(presPolys@data)
-desiredCols <- c("EO_ID_ST", "SNAME", "SCOMNAME", "RA", "OBSDATE")
+desiredCols <- c("EO_ID_ST", "SNAME", "SCOMNAME", "SFRACalc", "OBSDATE")
 if("FALSE" %in% c(desiredCols %in% shpColNms)) {
 	  stop(paste0("Column(s) are missing or incorrectly named: ", paste(desiredCols[!desiredCols %in% shpColNms], collapse = ", ")))
   } else {
     print("Required columns are present")
   }
-if(any(!complete.cases(presPolys@data[c("EO_ID_ST", "SNAME", "SCOMNAME", "RA")]))) {
-  stop("The columns 'EO_ID_ST','SNAME','SCOMNAME', and 'RA' (SFRACalc) cannot have NA values.")
+if(any(!complete.cases(presPolys@data[c("EO_ID_ST", "SNAME", "SCOMNAME", "SFRACalc")]))) {
+  stop("The columns 'EO_ID_ST','SNAME','SCOMNAME', and 'SFRACalc') cannot have NA values.")
 }
-#pare down columns
+
+# check if file already exists; if it does, stop and print error
+if (file.exists(paste0(baseName, ".shp"))) {
+  stop("A file already exists with that name: '", 
+       paste0(getwd(), "/", baseName, ".shp"), "'. Rename or delete it to continue.")
+} else {
+  writeOGR(presPolys, ".", layer = baseName, driver="ESRI Shapefile")
+}
+# set wd to inputs again, reload copied dataset
+setwd(paste0(loc_model,"/",model_species,"/inputs"))
+presPolys <- readOGR(dsn="presence", layer = baseName) #Z-dimension discarded msg is OK
+presPolys$RA <- presPolys$SFRACalc
+desiredCols <- c("EO_ID_ST", "SNAME", "SCOMNAME", "RA", "OBSDATE")
 presPolys@data <- presPolys@data[,desiredCols]
 
 # set date/year column to [nearest] year, rounding when day is given
@@ -104,14 +114,14 @@ shp_expl@data <- cbind(shp_expl@data,
 	AREAM2 = raster::area(shp_expl))
 
 # projection info doesn't stick, apply from what we grabbed earlier
-shp_expl@proj4string <- projInfo
+shp_expl@proj4string <- CRS(as.character(projInfo))
 #write out the exploded polygon set
-nm.PyFile <- paste(sppCode, "_expl", sep = "")
-writeOGR(shp_expl, dsn = ".", layer = nm.PyFile, driver="ESRI Shapefile", overwrite_layer=FALSE)
+nm.PyFile <- paste(baseName, "_expl", sep = "")
+writeOGR(shp_expl, dsn = "./model_input", layer = nm.PyFile, driver="ESRI Shapefile", overwrite_layer=FALSE)
   
 #name of random points output shapefile; add path to (now input) polygon file
-nm.RanPtFile <- paste(loc_spPts,"/", sppCode, "_RanPts", sep = "")
-nm.PyFile <- paste(loc_spPoly,"/", sppCode, "_expl", sep = "")
+nm.RanPtFile <- paste(baseName, "_RanPts", sep = "")
+nm.PyFile <- paste(baseName, "_expl", sep = "")
 
 ####
 ####  Placing random points within each sample unit (polygon/EO) ----
@@ -155,7 +165,7 @@ att.pt.2$finalSampNum <- ifelse(att.pt.2$PolySampNum < att.pt.2$minSamps,
 
 # add two more cols for later (backwards compatibility with old GRTS routine)
 att.pt.2$stratum <- att.pt.2$panelNum
-att.pt.2$siteID <- paste(sppCode, att.pt.2$expl_id, sep = "-")
+att.pt.2$siteID <- paste(baseName, att.pt.2$expl_id, sep = "-")
 # get these data into the spatial poly df
 shp_expl_dat <- merge(shp_expl, att.pt.2[,c("expl_id","finalSampNum","siteID","stratum")],
                       by.x = "EXPL_ID", by.y = "expl_id")
@@ -224,15 +234,15 @@ fullName <- paste(nm.RanPtFile,".shp",sep="")
 
 colsToKeep <- c("siteid", "stratum", tolower(desiredCols))
 ranPts <- ranPts[,colsToKeep]
-writeOGR(ranPts, dsn = fullName, layer = nm.RanPtFile, 
+writeOGR(ranPts, dsn = "./model_input", layer = nm.RanPtFile, 
 			driver="ESRI Shapefile", overwrite_layer=TRUE)
 
 # Write out various stats and data to the database ------
 # prep the data
-
-OutPut <- data.frame(SciName = paste(att.pt[1,"sname"]),
+OutPut <- data.frame(tableCode = baseName,
+  SciName = paste(att.pt[1,"sname"]),
 	CommName=paste(att.pt[1,"scomname"]),
-	ElemCode=sppCode,
+	ElemCode=model_species,
 	RandomPtFile=nm.RanPtFile,
 	date = paste(Sys.Date()),
 	time = format(Sys.time(), "%X"),
@@ -249,8 +259,7 @@ dbDisconnect(db)
 ###
 
 # get the background shapefile
-backgShapef <- readOGR(dsn=loc_bkgPts, layer=nm_bkgPts)
-
+backgShapef <- readOGR(dsn=dirname(nm_bkgPts), layer=gsub(".shp$", "", basename(nm_bkgPts)))
 #get projection info for later
 projInfo <- backgShapef@proj4string
 
@@ -269,6 +278,6 @@ backgSubset <- backgShapef[backgShapef@data$insideBuff == FALSE,]
 backgSubset@proj4string <- projInfo
 
 # write it out (note that `background` folder must exist in base species folder)---
-outFileName <- paste(nm_bkgPts, "_clean", sep="")
-writeOGR(backgSubset, dsn = "../background", layer = outFileName, 
+nm_bkgPts <- paste(baseName, "_clean", sep="")
+writeOGR(backgSubset, dsn = "model_input", layer = nm_bkgPts, 
          driver="ESRI Shapefile", overwrite_layer=TRUE)
--- 5_createMetadata.r
+++ 5_createMetadata.r
@@ -24,30 +24,25 @@ library(xtable)
 ## about line 35 where you choose which Rdata file to use,
 ## and about line 46 where you choose which record to use
 
-# loc_scripts <- "K:/Reg5Modeling_Project/scripts/Regional_SDM"
+setwd(loc_model)
+dir.create(paste0(model_species,"/outputs/metadata"), recursive = T, showWarnings = F)
+setwd(paste0(model_species,"/outputs"))
+load(paste0("rdata/", modelrun_meta_data$model_run_name,".Rdata"))
 
 # source(paste(loc_scripts, "0_pathsAndSettings.R", sep = "/"))
 
-# get a list of what's in the directory
-# d <- dir(path = loc_RDataOut, pattern = ".Rdata",full.names=FALSE)
-# d
-# # which one do we want to run?
-# n <- 1
-# fileName <- d[[n]]
-# load(paste(loc_RDataOut,fileName, sep="/"))
-setwd(loc_RDataOut)
-load(paste(modelrun_meta_data$model_run_name,".Rdata", sep=""))
-
 # get background poly data for the map
-referenceBoundaries <- readOGR(loc_otherSpatial, nm_refBoundaries, stringsAsFactors=FALSE) # name of state boundaries file
-studyAreaExtent <- readOGR(loc_otherSpatial, nm_studyAreaExtent, stringsAsFactors=FALSE) 
+layerdir <- dirname(nm_studyAreaExtent)
+layer <- strsplit(basename(nm_studyAreaExtent),"\\.")[[1]][[1]]
+studyAreaExtent <- readOGR(layerdir,  layer = layer) # study area
+# get background poly data for the map (study area, reference boundaries, and aquatic areas)
+layerdir <- dirname(nm_refBoundaries)
+layer <- strsplit(basename(nm_refBoundaries),"\\.")[[1]][[1]]
+referenceBoundaries <- readOGR(layerdir,  layer = layer) # name of state boundaries file
 
-r <- dir(path = loc_outRas, pattern = ".tif$",full.names=FALSE)
-# r
-# # which one do we want to run?
-# n <- 1
+r <- dir(path = "model_predictions", pattern = ".tif$",full.names=FALSE)
 fileName <- r[gsub(".tif", "", r) == model_run_name]
-ras <- raster(paste(loc_outRas, fileName, sep = "/"))
+ras <- raster(paste0("model_predictions/", fileName))
 
 ## Get Program and Data Sources info ----
 op <- options("useFancyQuotes")
@@ -55,18 +50,18 @@ options(useFancyQuotes = FALSE)
 
 db <- dbConnect(SQLite(),dbname=nm_db_file)  
 SQLquery <- paste("Select lkpModelers.ProgramName, lkpModelers.FullOrganizationName, ",
-  "lkpModelers.City, lkpModelers.State, lkpSpecies.CODE ",
-  "FROM lkpModelers ", 
-  "INNER JOIN lkpSpecies ON lkpModelers.ModelerID=lkpSpecies.ModelerID ", 
-  "WHERE lkpSpecies.CODE='", ElementNames$Code, "'; ", sep="")
+                  "lkpModelers.City, lkpModelers.State, lkpSpecies.CODE ",
+                  "FROM lkpModelers ", 
+                  "INNER JOIN lkpSpecies ON lkpModelers.ModelerID=lkpSpecies.ModelerID ", 
+                  "WHERE lkpSpecies.CODE='", model_species, "'; ", sep="")
 sdm.modeler <- dbGetQuery(db, statement = SQLquery)
 # NOTE: use column should be populated with 1/0 for sources of data used
-SQLquery <- paste("SELECT DISTINCT sp.CODE, sr.ProgramName, sr.State ",
-  "FROM lkpSpecies as sp ",
-  "INNER JOIN mapDataSourcesToSpp as mp ON mp.EstID=sp.EST_ID ",
-  "INNER JOIN lkpDataSources as sr ON mp.DataSourcesID=sr.DataSourcesID ",
-  "WHERE mp.use = 1 ",
-  "AND sp.CODE='", ElementNames$Code, "'; ", sep="")
+SQLquery <- paste("SELECT sp.CODE, sr.ProgramName, sr.State ",
+                  "FROM lkpSpecies as sp ",
+                  "INNER JOIN mapDataSourcesToSpp as mp ON mp.EstID=sp.EST_ID ",
+                  "INNER JOIN lkpDataSources as sr ON mp.DataSourcesID=sr.DataSourcesID ",
+                  "WHERE mp.use = 1 ",
+                  "AND sp.CODE='", model_species, "'; ", sep="")
 sdm.dataSources <- dbGetQuery(db, statement = SQLquery)
 sdm.dataSources <- sdm.dataSources[order(sdm.dataSources$ProgramName),]
 
@@ -124,12 +119,11 @@ sdm.thresh.table$Pts <- paste(round(sdm.thresh.table$Pts/numPts*100, 1),
 # Also, might need to run this twice. First time through tex builds the reference
 # list, second time through it can then number the refs in the doc.
 
-setwd(loc_outMetadata)
-
-#knit2pdf errors for some reason...just knit then call directly
-# knit2pdf(paste(loc_scripts,"MetadataEval_knitr.rnw",sep="/"), output=paste(model_run_name, ".tex",sep="")) 
+setwd("metadata")
+# knit2pdf errors for some reason...just knit then call directly
 knit(paste(loc_scripts,"MetadataEval_knitr.rnw",sep="/"), output=paste(model_run_name, ".tex",sep=""))
-call <- paste0("pdflatex -halt-on-error -interaction=nonstopmode ",model_run_name , ".tex")
+call <- paste0("pdflatex -interaction=nonstopmode ",model_run_name , ".tex")
+# call <- paste0("pdflatex -halt-on-error -interaction=nonstopmode ",model_run_name , ".tex") # this stops execution if there is an error. Not really necessary
 system(call)
 system(call) # 2nd run to apply citation numbers
 
