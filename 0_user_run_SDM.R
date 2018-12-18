3e10ada3ef737c872dba244257cfd0148ab79e7e
David Bucklin <david.bucklin@dcr.virginia.gov>
2018-12-18 14:47:52 -0500

    clean up 0_user_run_SDM, move picked-up runs to helper/0_user_run_SDM_rerun

--- 0_user_run_SDM.R
+++ 0_user_run_SDM.R
@@ -1,12 +1,10 @@
 # File: user_run_SDM.r
-# Purpose: Run a full SDM model, or pickup an existing run executed using run_SDM.
-# After running a full model, save this file in the species' 'loc_scripts' folder
+# Purpose: Run a new, full SDM model (all steps)
 
 library(here)
 rm(list=ls())
 
 # Step 1: Setting for the model run
-# set project folder, db, species code, and species reaches filename for this run
 
 # species code (from lkpSpecies in modelling database. This will be the new folder name containing inputs/ouptuts)
 model_species <- "micrmont"
@@ -33,9 +31,9 @@ metaData_comments = "bla bla"
 # your name
 modeller = "David Bucklin"
 
-# list non-standard variables to "add" to model run
+# list non-standard variables to add to model run
 add_vars = NULL
-# list standard variables to remove from model run
+# list standard variables to exclude from model run
 remove_vars = NULL
 # do you want to stop execution after each modeling step (script)?
 prompt = FALSE
@@ -51,11 +49,8 @@ source(here("helper", "run_SDM.R"))
 # Step 2: execute a new model
 # Usage: For a full, new model run, provide all paths/file names to arguments 'loc_scripts' THROUGH 'modeller'.
 # RUN A NEW MODEL (ALL STEPS 1-5)
-# If picking up from a previous run (after step 1), use Step 2-alt below
-# update the function arguments below as necessary, and run the function
 
 run_SDM(
-  begin_step = "1",
   model_species = model_species, # species code in DB; new folder to create in loc_model if not existing
   loc_scripts = loc_scripts, 
   nm_presFile = nm_presFile,
@@ -73,61 +68,6 @@ run_SDM(
   prompt = prompt
 )
 
-#############################################################################
-#############################################################################
-#############################################################################
-
-# Step 2-alt: run additional model, or pick up from previous model run
-
-# if using add_vars or remove_vars for a new model run, start at step 2.
-
-# if you want to run a new model with the same input data as a previous run, start at step 3.
-
-# If picking up from a previously started run, always
-# provide the begin_step, model_species, and loc_model.
-# When starting at script #4 or later, also provide the name of the 
-# model rdata file to 'model_rdata'. 
-# You can also include any other arguments that you wish to change from 
-# the previous run (e.g., model_comments or metaData_comments).
-# 
-# Note that you can manually update the scripts, if desired. 
-# The scripts will automatically be accessed from 'loc_scripts' (if provided) 
-# or the location that was specified for the original model run. 
-library(here)
-rm(list=ls())
-
-# set project folder and species code for this run
-model_species <- "micrmont"
-loc_model <- here("_data", "species")
-
-# set wd and load function
-loc_scripts <- here()
-setwd(loc_scripts)
-source(here("helper", "run_SDM.R"))
-
-# example pick-up a model run at step 3 (new model, same presence/bkgd data)
-  # if starting at step 2/3, provide an input tableCode to nm_presFile 
-  # to add/remove vars, begin at step 2
-  # to just run new model, begin at step 3
-run_SDM(
-  begin_step = "2",
-  model_species = "micrmont",
-  loc_model = loc_model,
-  nm_presFile = "micrmont_20181113_151538",
-  remove_vars = "elevx10"
-)
-
-# example pick-up a model run at step 4c (metadata/comment update)
-  # if starting at step 4 or later, must provide model run name to model_rdata
-run_SDM(
-  begin_step = "4c",
-  model_species = "micrmont",
-  loc_model = loc_model,
-  model_rdata = "micrmont_20181017_124730",
-  metaData_comments = "UPDATED METADATA COMMENT"
-)
-
-
 ########## 
 ##########
 ##########
@@ -137,7 +77,7 @@ library(here)
 rm(list=ls())
 # Use the lines below for debugging (running line by line) for a certain script
 # This loads the variables used in previous model run for the species, 
-# so you need to have executed run_SDM in step 2 first.
+# so you need to have started a run_SDM() run in step 2 first.
 
 # for scripts 1-3, run just the following 3 lines
 model_species <- "micrmont"
@@ -147,4 +87,3 @@ for(i in 1:length(fn_args)) assign(names(fn_args)[i], fn_args[[i]])
 # if debugging script 4 or later, also load the specific model output rdata file
 model_rdata <- max(list.files(here("_data","species",model_species,"outputs","rdata")))
 load(here("_data","species",model_species,"outputs","rdata",paste0(model_rdata)))
-
