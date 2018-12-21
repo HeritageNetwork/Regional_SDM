b6c2aeaa3098130b6bd941d66264ad87205c2810
ChristopherTracey <ctracey@paconserve.org>
2018-12-21 12:54:47 -0500

    misc updates

--- helper/pkg_check.R
+++ helper/pkg_check.R
@@ -4,7 +4,7 @@
 pkg_list <- c("RSQLite","rgdal","sp","sf","rgeos","raster","maptools",
               "ROCR","vcd","abind","foreign","randomForest",
               "snow", "DBI", "knitr","RColorBrewer","rasterVis","xtable",
-              "git2r","spsurvey","classInt","tinytex", "stringi")
+              "git2r","spsurvey","classInt","tinytex", "stringi","tables")
 installed <- installed.packages()
 to_inst <- pkg_list[!pkg_list %in% installed[,1]]
 
