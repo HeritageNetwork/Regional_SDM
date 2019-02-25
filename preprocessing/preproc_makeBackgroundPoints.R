d3d0fd7bc48cc698bbc8dc805315a9f86cf4d998
Tim Howard <tghoward@gmail.com>
2019-02-25 14:17:38 -0500

    WIP background points

--- preprocessing/preproc_makeBackgroundPoints.R
+++ preprocessing/preproc_makeBackgroundPoints.R
@@ -9,50 +9,44 @@ library(RSQLite)
 
 # temp - tgh - make a shapefile of study area based on HUC
 # get range info from the DB (as a list of HUCs)
-model_species <- "amazviri"
-nm_db_file <- here("_data", "databases", "SDM_lookupAndTracking.sqlite")
-db <- dbConnect(SQLite(),dbname=nm_db_file)
-SQLquery <- paste0("SELECT huc10_id from lkpRange
-                   inner join lkpSpecies on lkpRange.EGT_ID = lkpSpecies.EGT_ID
-                   where lkpSpecies.sp_code = '", model_species, "';")
-hucList <- dbGetQuery(db, statement = SQLquery)$huc10_id
-dbDisconnect(db)
-rm(db)
-# get the huc10 layer
-nm_HUC_pth <- here("_data","other_spatial","feature")
-nm_HUC_file <- "HUC10.shp"
-
-op <- options()
-options(useFancyQuotes = FALSE) #need straight quotes for query
-qry <- paste0("SELECT * FROM \"HUC10\" where HUC10 IN (", paste(sQuote(hucList), collapse = ", ", sep = "")," )")
-rng <- st_read(nm_HUC_pth, nm_HUC_file,
-               query = qry)
-options(op)
-rm(op)
-
-sa <- st_union(rng)
-
+# model_species <- "amazviri"
+# nm_db_file <- here("_data", "databases", "SDM_lookupAndTracking.sqlite")
+# db <- dbConnect(SQLite(),dbname=nm_db_file)
+# SQLquery <- paste0("SELECT huc10_id from lkpRange
+#                    inner join lkpSpecies on lkpRange.EGT_ID = lkpSpecies.EGT_ID
+#                    where lkpSpecies.sp_code = '", model_species, "';")
+# hucList <- dbGetQuery(db, statement = SQLquery)$huc10_id
+# dbDisconnect(db)
+# rm(db)
+# # get the huc10 layer
+# nm_HUC_pth <- here("_data","other_spatial","feature")
+# nm_HUC_file <- "HUC10.shp"
+# 
+# op <- options()
+# options(useFancyQuotes = FALSE) #need straight quotes for query
+# qry <- paste0("SELECT * FROM \"HUC10\" where HUC10 IN (", paste(sQuote(hucList), collapse = ", ", sep = "")," )")
+# rng <- st_read(nm_HUC_pth, nm_HUC_file,
+#                query = qry)
+# options(op)
+# rm(op)
+# 
+# sa <- st_union(rng)
+# 
 
 # background points shapefile (points are generated within this boundary). 
 # this should be in the project's projection (matching the raster EVs)
-#StudyAreaPoly <- "E:/mobi_repo_tgh_clean2/Regional_SDM/_data/species/anaxexsu/inputs/model_input/anaxexsu_20190110_121839_studyArea.gpkg"
-#sa <- st_read(StudyAreaPoly)
 
-
-# setwd("E:/mobi_repo_tgh_clean2/Regional_SDM/_data/other_spatial/feature")
-# 
-# states <- st_read("US_States.shp")
-# 
-# states_diss <- st_union(states)
-# 
-# sa <-  states_diss
-
+setwd(here("_data","other_spatial","feature"))
+states <- st_read("US_States.shp")
+#study area is a dissolved representation of CONUS
+sa <- st_union(states)
 
 # huc12s. These polygons are used to attribute points. Must have "HUC_12" column.
 huc12 <- "N:/rangestuff/HUC_ref/huc12_fromChris/huc12.shp"
+
 # number of points to generate
-#numpts <- 1000000
-numpts <- 7000
+numpts <- 1000000
+
 # new/existing db table name (overwritten)
 table <- "background_pts"
 
@@ -64,7 +58,11 @@ pathToTab <- here("_data","env_vars","tabular")
 # load layers
 #sa <- st_read(StudyAreaPoly)
 crs <- st_crs(sa)
-huc <- st_transform(st_read(huc12), crs)
+huc <- st_read(huc12)
+
+if (crs != st_crs(huc)){
+  huc <- st_transform(st_read(huc12), crs)  
+}
 
 # generate points
 samps1 <- st_sample(sa, size = numpts)
@@ -76,7 +74,7 @@ names(samps) <- c("fid", "huc12", "geometry")
 sampsDF <- data.frame(fid = samps$fid, huc12 = samps$huc12, wkt = st_as_text(samps$geometry))
 
 # send to database
-db <- dbConnect(SQLite(), paste0(pathToTab, "/", "background_amazviri.sqlite"))
+db <- dbConnect(SQLite(), paste0(pathToTab, "/", "background_conus.sqlite"))
 tp <- as.vector("INTEGER")
 names(tp) <- "fid"
 dbWriteTable(db, table, sampsDF, overwrite = T, field.types = tp)
