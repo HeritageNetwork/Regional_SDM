70600c8787a5f76779b7b96b3e74bd053e175f98
Tim Howard <tghoward@gmail.com>
2019-02-27 08:46:24 -0500

    WIP: handle millions of points, CONUS

--- preprocessing/preproc_attributeBackgroundPts.R
+++ preprocessing/preproc_attributeBackgroundPts.R
@@ -7,6 +7,7 @@ library(raster)
 library(sf)
 library(here)
 library(RSQLite)
+library(snowfall)
 
 # path where .tif env. var rasters are stored
 pathToRas <- here("_data","env_vars","raster","ras")
@@ -49,9 +50,10 @@ gridlist <- gridlist[order(names(gridlist))]
 names(gridlist) <- shortNames[order(shortNames$fileName),"gridName"]
 
 nulls <- gridlist[is.na(names(gridlist))]
-
-# if any records here, stop! there are some cases of no match in the DB
-nulls
+if(length(nulls) > 0){
+  print(nulls)
+  stop("Some grids are not in DB.")
+}
 
 envStack <- stack(gridlist)
 # end_time <- Sys.time()
@@ -71,48 +73,91 @@ envStack <- stack(gridlist)
 # end_time <- Sys.time()
 # end_time - start_time
 
-## Get random points table ----
-db <- dbConnect(SQLite(), paste0(pathToTab, "/", "background_amazviri.sqlite"))
-bkgd <- dbReadTable(db, table)
-
-#remove any huc12 nulls
-bkgd <- bkgd[complete.cases(bkgd),]
-
-tcrs <- dbGetQuery(db, paste0("SELECT proj4string p from lkpCRS where table_name = '", table, "';"))$p
-samps <- st_sf(bkgd, geometry = st_as_sfc(bkgd$wkt, crs = tcrs))
-
-#### exploring multi-core options
-#https://gis.stackexchange.com/questions/253618/r-multicore-approach-to-extract-raster-values-using-spatial-points
-library(snowfall)
-# Extract values to a data frame - multicore approach
-# First, convert raster stack to list of single raster layers
 s.list <- unstack(envStack)
 names(s.list) <- names(envStack)
 
-# Now, create a R cluster using all the machine cores minus one
-sfInit(parallel=TRUE, cpus=parallel:::detectCores()-1)
+## Get random points table ----
+db <- dbConnect(SQLite(), paste0(pathToTab, "/", "background_CONUS.sqlite"))
 
-# Load the required packages inside the cluster
-sfLibrary(raster)
-sfLibrary(sf)
+## read in 1 million at a time
+# sql_countRows <- paste0("SELECT COUNT(*) AS c from ", table, ";")
+# countRows <- dbGetQuery(db, sql_countRows)
+# loops <- ceiling(countRows/1000000)
 
-# Run parallelized 'extract' function and stop cluster
-e.df <- sfSapply(s.list, extract, y=samps, method = "simple")
-sfStop()
+tcrs <- dbGetQuery(db, paste0("SELECT proj4string p from lkpCRS where table_name = '", table, "';"))$p
 
-DF <- data.frame(e.df)
+# get all the points
+bkgd <- dbReadTable(db, table)
+bkgd <- bkgd[complete.cases(bkgd),]
 
-
-
-# extract values
-#x <- extract(envStack, samps, method="simple")
-sampsAtt <- as.data.frame(cbind(fid = as.integer(samps$fid), DF))
-
-# write to DB
+# attribute and write the table with two points
+bkgd_subs <- bkgd[1:2,]
+samps <- st_sf(bkgd_subs, geometry = st_as_sfc(bkgd_subs$wkt, crs = tcrs))
+att <- extract(envStack, samps, method="simple")
+sampsAtt <- as.data.frame(cbind(fid = as.integer(samps$fid), att))
 tp <- as.vector("INTEGER")
 names(tp) <- "fid"
-dbWriteTable(db, paste0(table, "_att"), sampsAtt, overwrite = T, field.types = tp)
-# not writing shapefile, since base shapefile already exists
+dbWriteTable(db, paste0(table, "_att"), sampsAtt, overwrite = TRUE, field.types = tp)
+
+# now loop through the rest
+bkgd <- bkgd[-c(1:2),]
+brks <- cut(bkgd$fid, breaks = 30)
+brkgrps <- unique(brks)
+
+
+for(i in 1:length(brkgrps)){
+  bg <- bkgd[brks == brkgrps[i],]
+  samps <- st_sf(bg, geometry = st_as_sfc(bg$wkt, crs = tcrs))
+
+  # create an R cluster using all the machine cores minus two
+  sfInit(parallel=TRUE, cpus=parallel:::detectCores()-2)
+  # Load the required packages inside the cluster
+  sfLibrary(raster)
+  sfLibrary(sf)
+  # Run parallelized 'extract' function and stop cluster
+  e.df <- sfSapply(s.list, extract, y=samps, method = "simple")
+  sfStop()
+  DF <- data.frame(e.df)
+  sampsAtt <- as.data.frame(cbind(fid = as.integer(samps$fid), DF))
+  # write to DB
+  dbWriteTable(db, paste0(table, "_att"), sampsAtt, overwrite = FALSE, append = TRUE)
+  print(paste0("done with ", i, " of 30 loops"))
+}
+
+
+# without looping
+# bkgd <- dbReadTable(db, table)
+# 
+# #remove any huc12 nulls
+# bkgd <- bkgd[complete.cases(bkgd),]
+# 
+# tcrs <- dbGetQuery(db, paste0("SELECT proj4string p from lkpCRS where table_name = '", table, "';"))$p
+# samps <- st_sf(bkgd, geometry = st_as_sfc(bkgd$wkt, crs = tcrs))
+
+
+#### multi-core !
+# https://gis.stackexchange.com/questions/253618/r-multicore-approach-to-extract-raster-values-using-spatial-points
+# Extract values to a data frame - multicore approach
+# First, convert raster stack to list of single raster layers
+# s.list <- unstack(envStack)
+# names(s.list) <- names(envStack)
+# # Now, create a R cluster using all the machine cores minus one
+# sfInit(parallel=TRUE, cpus=parallel:::detectCores()-1)
+# # Load the required packages inside the cluster
+# sfLibrary(raster)
+# sfLibrary(sf)
+# # Run parallelized 'extract' function and stop cluster
+# e.df <- sfSapply(s.list, extract, y=samps, method = "simple")
+# sfStop()
+# 
+# DF <- data.frame(e.df)
+# sampsAtt <- as.data.frame(cbind(fid = as.integer(samps$fid), DF))
+# 
+# # write to DB
+# tp <- as.vector("INTEGER")
+# names(tp) <- "fid"
+# dbWriteTable(db, paste0(table, "_att"), sampsAtt, overwrite = T, field.types = tp)
+# # not writing shapefile, since base shapefile already exists
 
 dbDisconnect(db)
 rm(db)
