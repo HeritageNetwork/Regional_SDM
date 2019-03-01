948af80a15dacca43d24e510392df3808b2264a6
Tim Howard <tghoward@gmail.com>
2019-03-01 12:31:34 -0500

    declutter: move rubric update to 4c

--- 5_createMetadata.r
+++ 5_createMetadata.r
@@ -17,7 +17,6 @@ library(xtable)
 library(tinytex)
 library(stringi)
 library(tables)
-library(RODBC)
 
 ### find and load model data ----
 ## three lines need your attention. The one directly below (loc_scripts),
@@ -119,80 +118,6 @@ grank_desc <- dbGetQuery(db, SQLquery)
 
 # make a url to NatureServe Explorer
 NSurl <- paste("http://explorer.natureserve.org/servlet/NatureServe?searchName=",gsub(" ", "+", ElementNames[[1]], fixed=TRUE), sep="")
-
-## update Model Evaluation and Use data ----
-# get most recent data from the tracking DB for two of the rubric table fields that may vary
-cn <- odbcConnect("mobi_spp_tracking")
-sql <- paste0("SELECT FinalSppList.ELEMENT_GLOBAL_ID, LocalityData.pres_dat_eval_rubric, 
-    LocalityData.bison_use, LocalityData.gbif_use, LocalityData.inat_use, 
-    LocalityData.other_use, LocalityData.MJD_sufficient, LocalityData.MJD_only, LocalityData.status
-  FROM FinalSppList INNER JOIN LocalityData ON FinalSppList.ELEMENT_GLOBAL_ID = LocalityData.EGT_ID
-  WHERE (((FinalSppList.ELEMENT_GLOBAL_ID)= ", ElementNames$EGT_ID, "));")
-localityData <- sqlQuery(cn, sql)
-
-sql <- paste0("SELECT Reviewer.EGT_ID, Reviewer.response, Reviewer.date_completed
-              FROM Reviewer
-              WHERE (((Reviewer.EGT_ID)= ", ElementNames$EGT_ID, " ));")
-reviewerData <- sqlQuery(cn, sql)
-close(cn)
-rm(cn)
-
-if(localityData$status == "complete"){
-  # if complete, take scoring assessment from DB
-  #1 = caution, 2 = acceptible, 3 = ideal
-  dataQuality <- localityData$pres_dat_eval_rubric
-} else if(sum(localityData[,c("bison_use","gbif_use","inat_use","other_use")]) > 0) {
-  if(localityData$MJD_only == 1){
-      #this really means 'mjd_use' in the DB, not 'mjd_only'
-      # if any of the BIG data are tagged for use as well as the MJD data, tag it acceptible
-      dataQuality <- 2
-    } else {
-      # if no MJD data, tag as caution
-      dataQuality <- 1
-  }
-} else if(!sum(localityData[,c("bison_use","gbif_use","inat_use","other_use")]) > 0) {
-  if(localityData$MJD_only == 1){ 
-    #this really means 'mjd_use' in the DB, not 'mjd_only'
-    # if only MJD data tagged to use
-    dataQuality <- 3
-  } else {
-    #if nothing is tagged in DB, assume a mix of records
-    dataQuality <- 2
-  }
-}
-dqMatrix <- data.frame("dataQuality" = c(1,2,3),
-              "dqAttribute" = c("C","A","I"),
-              "dqComments" = c("Data taken from outside sources and may or may not be vetted for accuracy or weighted for spatial representation.",
-                               "Heritage Network data augmented with outside data which may or may not be vetted for accuracy or weighted for spatial representation.",
-                               "Heritage Network data are vetted for accuracy and weighted for spatial representation."))
-dqUpdate <- dqMatrix[match(dataQuality, dqMatrix$dataQuality),]
-#push it up to DB
-sql <- paste0("update lkpSpeciesRubric set spdata_dataqual = '", dqUpdate$dqAttribute, 
-              "', spdata_dataqualNotes = '", dqUpdate$dqComments, 
-              "' where EGT_ID = ", ElementNames$EGT_ID, " ;")
-dbExecute(db, statement = sql)
-## performance
-prfmcMatrix <- data.frame("pAttribute" = c("C","A"),
-                          "pComments" = c("Model TSS < 0.6. Mapped model output is evaluated for ecological plausibility by expert review.",
-                                          "Model TSS >= 0.6. Mapped model output is evaluated for ecological plausibility by expert review."))
-prfmAtt <- ifelse(tss.summ$mean<=0.6, "C", "A")
-prfmUpdate <- prfmcMatrix[match(prfmAtt, prfmcMatrix$pAttribute),]
-sql <- paste0("update lkpSpeciesRubric set process_perform = '", prfmUpdate$pAttribute, 
-              "', process_performNotes = '", prfmUpdate$pComments, 
-              "' where EGT_ID = ", ElementNames$EGT_ID, " ;")
-dbExecute(db, statement = sql)
-## model review
-modRev <- sum(reviewerData$response)
-revMatrix <- data.frame("rAttribute" = c("C","A"),
-                        "rComments" = c("Model was not reviewed by regional, taxonomic experts.",
-                                        "Model was reviewed by regional, taxonomic experts."))
-revAtt <- ifelse(sum(reviewerData$response) > 0 , "A", "C")
-revUpdate <- revMatrix[match(revAtt, revMatrix$rAttribute),]
-sql <- paste0("update lkpSpeciesRubric set process_review = '", revUpdate$rAttribute, 
-              "', process_reviewNotes = '", revUpdate$rComments, 
-              "' where EGT_ID = ", ElementNames$EGT_ID, " ;")
-dbExecute(db, statement = sql)
-
 
 ## get Model Evaluation and Use data ----
 SQLquery <- paste("Select spdata_dataqual, spdata_abs, spdata_eval, envvar_relevance, envvar_align, process_algo, process_sens, process_rigor, process_perform, process_review, products_mapped, products_support, products_repo, interative, spdata_dataqualNotes, spdata_absNotes, spdata_evalNotes, envvar_relevanceNotes, envvar_alignNotes, process_algoNotes, process_sensNotes, process_rigorNotes, process_performNotes, process_reviewNotes, products_mappedNotes, products_supportNotes, products_repoNotes, interativeNotes ", 
