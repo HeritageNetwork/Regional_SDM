c9b32d70d80ed5a7a34358f216c39eefb86e2583
Christopher Tracey <ChristopherTracey@users.noreply.github.com>
2019-03-11 16:31:47 -0400

    add tmap tmaptools and OpenStreetMap to pkgcheck

--- helper/pkg_check.R
+++ helper/pkg_check.R
@@ -4,7 +4,7 @@
 pkg_list <- c("RSQLite","rgdal","sp","rgeos","raster","maptools",
               "ROCR","vcd","abind","foreign","randomForest",
               "snow", "DBI", "knitr","RColorBrewer","rasterVis","xtable",
-              "git2r","spsurvey", "here","sf","dplyr","stringi")
+              "git2r","spsurvey", "here","sf","dplyr","stringi","tmap","tmaptools","OpenStreetMap")
 installed <- installed.packages()
 to_inst <- pkg_list[!pkg_list %in% installed[,1]]
 
