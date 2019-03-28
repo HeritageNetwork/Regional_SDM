93196e932885efb148a0beb0bf0560a907ddbfab
Tim Howard <tghoward@gmail.com>
2019-03-28 13:28:54 -0400

    update package list

--- helper/pkg_check.R
+++ helper/pkg_check.R
@@ -4,7 +4,8 @@
 pkg_list <- c("RSQLite","rgdal","sp","rgeos","raster","maptools",
               "ROCR","vcd","abind","foreign","randomForest",
               "snow", "DBI", "knitr","RColorBrewer","rasterVis","xtable",
-              "git2r","spsurvey", "here","sf","dplyr","stringi","tmap","tmaptools","OpenStreetMap")
+              "git2r","spsurvey", "here","sf","dplyr","stringi","tmap","tmaptools","OpenStreetMap",
+              "snowfall", "smoothr", "tables","rJava", "tinytex")
 installed <- installed.packages()
 to_inst <- pkg_list[!pkg_list %in% installed[,1]]
 
