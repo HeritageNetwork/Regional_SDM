63db509e0f8b0b733790ce59589022788fc626c0
Tim Howard <tghoward@gmail.com>
2019-04-05 08:17:01 -0400

    parLapply tweaks for random errors

--- helper/crop_mask_rast.R
+++ helper/crop_mask_rast.R
@@ -91,7 +91,7 @@ clusterExport(cl, list("temp", "ext", "clipshp"), envir = environment())
 clusterExport(cl, list("loc_envVars"), envir = environment()) 
 
 message("Creating raster subsets for species for ", length(fullL) , " environmental variables...")
-newL <- parLapply(cl, x = fullL, fun = function(path) {
+newL <- parLapply(cl, X = fullL, fun = function(path) {
   subnm <- gsub(paste0(loc_envVars,"/"), "", path)
   if (grepl("/",subnm)) {
     subdir <- strsplit(subnm, "/", fixed = T)[[1]][1]
