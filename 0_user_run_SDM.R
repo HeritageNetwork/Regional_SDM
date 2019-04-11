66eaf6cf305d68cba33e99ef60c527bd440d6fe4
Tim Howard <tghoward@gmail.com>
2019-04-11 12:00:26 -0400

    spp change

--- 0_user_run_SDM.R
+++ 0_user_run_SDM.R
@@ -8,7 +8,7 @@ rm(list=ls())
 
 # species code (from lkpSpecies in modelling database. This will be the new folder name containing inputs/ouptuts)
 
-model_species <- "callirus"
+model_species <- "pycnclin"
 # loc_scripts is your repository. Make sure your git repository is set to correct branch
 loc_scripts <- here()
 # The main modelling folder for inputs/outputs. All sub-folders are created during the model run (when starting with step 1)
@@ -163,7 +163,7 @@ rm(list=ls())
 
 # for scripts 1-3, run just the following 3 lines
 
-model_species <- "amazviri"
+model_species <- "pycnclin"
 load(here("_data","species",model_species,"runSDM_paths.Rdata"))
 for(i in 1:length(fn_args)) assign(names(fn_args)[i], fn_args[[i]])
 
