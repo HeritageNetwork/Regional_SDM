62bb8355e1d54ddf259a392687bc5a9fadb84d7c
Tim Howard <tghoward@gmail.com>
2019-10-07 09:21:26 -0400

    model review table labeling

--- postReview_createMetadata.r
+++ postReview_createMetadata.r
@@ -199,6 +199,12 @@ revAtt <- ifelse(nrow(reviewData) == 0 , "C",
                         ifelse(meanRating < 3.5, "A", "I")))
 revUpdate <- revMatrix[match(revAtt, revMatrix$rAttribute),]
 
+## get Model Evaluation and Use data ----
+SQLquery <- paste("Select spdata_dataqual, spdata_abs, spdata_eval, envvar_relevance, envvar_align, process_algo, process_sens, process_rigor, process_perform, process_review, products_mapped, products_support, products_repo, interative, spdata_dataqualNotes, spdata_absNotes, spdata_evalNotes, envvar_relevanceNotes, envvar_alignNotes, process_algoNotes, process_sensNotes, process_rigorNotes, process_performNotes, process_reviewNotes, products_mappedNotes, products_supportNotes, products_repoNotes, interativeNotes ", 
+                  "FROM lkpSpeciesRubric ", 
+                  "WHERE sp_code ='", model_species, "'; ", sep="")
+sdm.modeluse <- dbGetQuery(db, statement = SQLquery)
+
 sdm.modeluse$process_review <- as.character(revUpdate$rAttribute)
 sdm.modeluse$process_reviewNotes <- as.character(revUpdate$rComments)
 
