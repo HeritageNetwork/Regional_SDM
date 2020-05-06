6f56bd1ba94bddfaa1d3f6cee6344297df28a5be
Tim Howard <tghoward@gmail.com>
2020-05-06 14:53:08 -0400

    first version xgb model

--- ensemble/xgb_3_createModel.R
+++ ensemble/xgb_3_createModel.R
@@ -19,292 +19,42 @@ library(recipes)
 # for xgb
 library(xgboost)
 
-source(paste0(loc_scripts, "/helper/modelrun_meta_data.R"), local = FALSE) # generates modelrun_meta_data
-
-setwd(loc_model)
-dir.create(paste0(model_species,"/outputs/rdata"), recursive = T, showWarnings = F)
-setwd(paste0("./",model_species,"/inputs"))
-
-# read data from the att db
-dbName <- paste(baseName, "_att.sqlite", sep="")
-db <- dbConnect(SQLite(), paste0("model_input/",dbName))
-
-tableName <- paste0(baseName, "_att")
-df.in <- dbReadTable(db, tableName)
-
-# get the background data from the DB
-tableName <- paste0(nm_bkgPts[2], "_clean")
-df.abs <- dbReadTable(db, tableName)
-dbDisconnect(db)
-rm(db, dbName, tableName)
-
-# set the seed before validation loops
-set.seed(seed)
-
-# connect to DB ..
-db <- dbConnect(SQLite(),dbname=nm_db_file)
-# get species info
-SQLquery <- paste("SELECT scientific_name SciName, common_name CommName, sp_code Code, broad_group Type, egt_id, g_rank, rounded_g_rank FROM lkpSpecies WHERE sp_code = '", model_species,"';", sep="")
-ElementNames <- as.list(dbGetQuery(db, statement = SQLquery)[1,])
-
-# write model input data to database before any other changes made
-tblModelInputs <- data.frame(table_code = baseName, EGT_ID = NA, datetime = as.character(Sys.time()),
-                             feat_count = length(unique(df.in$stratum)), 
-                             feat_grp_count = length(unique(df.in$group_id)), 
-                             obs_count = length(df.in[,1]), bkgd_count = length(df.abs[,1]),
-                             range_area_sqkm = NA)
-dbExecute(db, paste0("DELETE FROM tblModelInputs where table_code = '", baseName, "';")) # remove any previously prepped dataset entry
-dbWriteTable(db, "tblModelInputs", tblModelInputs, append = T)
-envvar_list <- dbGetQuery(db, "SELECT gridname g from lkpEnvVars;")$g
-envvar_list <- tolower(envvar_list)
-
-#also get correlated env var information
-SQLquery <- "SELECT gridName, correlatedVarGroupings FROM lkpEnvVars WHERE correlatedVarGroupings IS NOT NULL order by correlatedVarGroupings;"
-corrdEVs <- dbGetQuery(db, statement = SQLquery)
-
-dbDisconnect(db)
-rm(db, SQLquery)
-
-# are we using the 330 m raster set? if so, rename df.abs
-# all 330m raster names begin with "z3"
-if(length(grep("z3",names(df.in))) > 0){
-  db <- dbConnect(SQLite(),dbname=nm_db_file)
-  sql <- "SELECT names_30m, names_330m from mapEnvVarDifferentResolutions;"
-  envarNames <- dbGetQuery(db, statement = sql, stringsAsFactors = FALSE) 
-  envarNames <- data.frame(sapply(envarNames, FUN = function(x) tolower(x)), stringsAsFactors = FALSE)
-  names(df.abs) <- tolower(names(df.abs))
-  namesDF <- data.frame(absNames = names(df.abs), stringsAsFactors = FALSE)
-  namesDF <- merge(namesDF, envarNames, by.x = "absNames", by.y = "names_30m", all.x = TRUE)
-  namesDF$names_330m[is.na(namesDF$names_330m)] <- namesDF$absNames[is.na(namesDF$names_330m)]
-  # order them, then rename them
-  df.abs <- df.abs[,namesDF$absNames]
-  names(df.abs) <- namesDF$names_330m
-  dbDisconnect(db)
-  rm(db, sql, envarNames, namesDF)
-  }
-
-#make sure we don't have any NAs
-df.in <- df.in[complete.cases(df.in[,!names(df.in) %in% c("obsdate","date")]),]  # to ensure missing dates are not excluding records
-df.abs <- df.abs[complete.cases(df.abs),]
-
-# align data sets, QC ----
-# add some fields to each
-df.in <- cbind(df.in, pres=1)
-df.abs$stratum <- "pseu-a"
-df.abs <- cbind(df.abs, GROUP_ID="pseu-a", 
-					pres=0, RA="high", SPECIES_CD="background")
-
-# lower case column names
-names(df.in) <- tolower(names(df.in))
-names(df.abs) <- tolower(names(df.abs))
-
-# get an original list of env-vars for later writing to tblVarsUsed
-envvar_list <- names(df.abs)[names(df.abs) %in% envvar_list] # gets a list of environmental variables
-
-# get a list of env vars from the folder used to create the raster stack
-raslist <- list.files(path = loc_envVars, pattern = ".tif$", recursive = TRUE)
-
-# get short names from the DB
-# first shorten names in subfolders (temporal vars).
-raslist.short <- unique(unlist(
-  lapply(strsplit(raslist, "/"), function(x) {x[length(x)]})
-))
-
-db <- dbConnect(SQLite(),dbname=nm_db_file)
-SQLQuery <- "select gridName, fileName from lkpEnvVars;"
-evs <- dbGetQuery(db, SQLQuery)
-# restrict to rasters in folder
-shrtNms <- merge(data.frame(fileName = raslist.short), evs)
-
-# get the env vars used by df.in
-# assumes all env vars in df.in are accounted for by DB and in ras folders
-shrtNms <- shrtNms[tolower(shrtNms$gridName) %in% names(df.in),]
-
-# trust that the desired env vars are in df.in
-rasnames <- tolower(shrtNms$gridName)
-
-# get a list of all distance-to env vars
-SQLquery <- "SELECT gridName FROM lkpEnvVars WHERE distToGrid = 1;"
-dtGrids <- dbGetQuery(db, statement = SQLquery)
-
-# clean up
-#options(op)
-dbDisconnect(db)
-rm(db, SQLQuery, SQLquery, shrtNms)
-
-# Remove irrelevant distance-to grids ----
-# check if pres points are VERY far away from any of the dist-to grids
-#   (this can cause erroneous, non-biological relationships that should
-#    not be driving the model. Group decision to remove.)
-
-# get the ones we are using here
-dtRas <- rasnames[rasnames %in% tolower(dtGrids$gridName)]
-# what's the closest distance for each?
-dtRas.min <- apply(as.data.frame(df.in[,dtRas]), 2, min)
-# remove those whose closest distance is greater than 10km
-dtRas.sub <- dtRas.min[dtRas.min > 5000]
-rasnames <- rasnames[!rasnames %in% names(dtRas.sub)]
-
-rm(dtRas, dtRas.min, dtRas.sub)
-
-# clean up, merge data sets -----
-# this is the full list of fields, arranged appropriately
-colList <- c("species_cd","group_id","pres","stratum", "ra", rasnames)
-
-# if colList gets modified, 
-# also modify the locations for the independent and dependent variables, here
-depVarCol <- 3
-indVarCols <- c(6:length(colList))
-
-#re-arrange
-df.in <- df.in[,colList]
-df.abs <- df.abs[,colList]
-
-# row bind the pseudo-absences with the presence points
-df.abs$group_id <- factor(df.abs$group_id)
-df.full <- rbind(df.in, df.abs)
-
-# reset these factors
-df.full$stratum <- factor(df.full$stratum)
-df.full$group_id <- factor(df.full$group_id)
-df.full$pres <- factor(df.full$pres)
-df.full$ra <- factor(tolower(as.character(df.full$ra)))
-df.full$species_cd <- factor(df.full$species_cd)
-
-#how many polygons do we have?
-numPys <-  nrow(table(df.in$stratum))
-#how many EOs do we have?
-numEOs <- nrow(table(df.in$group_id))
-
-#initialize the grouping list, and set up grouping variables
-#if we have fewer than 5 EOs, move forward with jackknifing by polygon, otherwise
-#jackknife by EO.
-group <- vector("list")
-group$colNm <- ifelse(numEOs < 5,"stratum","group_id")
-group$JackknType <- ifelse(numEOs < 5,"polygon","spatial grouping")
-if(numEOs < 5) {
-  group$vals <- unique(df.in$stratum)
-} else {
-  group$vals <- unique(df.in$group_id)
-}
-
-# make samp size groupings ----
-EObyRA <- unique(df.full[,c(group$colNm,"ra")])
-EObyRA$sampSize[EObyRA$ra == "very high"] <- 5
-EObyRA$sampSize[EObyRA$ra == "high"] <- 4
-EObyRA$sampSize[EObyRA$ra == "medium"] <- 3
-EObyRA$sampSize[EObyRA$ra == "low"] <- 2
-EObyRA$sampSize[EObyRA$ra == "very low"] <- 1
-# set the background pts to the sum of the EO samples
-# EObyRA$sampSize[EObyRA$group_id == "pseu-a"] <- sum(EObyRA[!EObyRA$group_id == "pseu-a", "sampSize"])
-
-# there appear to be cases where more than one 
-# RA is assigned per EO. Handle it here by 
-# taking max value
-EObySS <- aggregate(EObyRA$sampSize, by=list(EObyRA[,group$colNm]), max)
-# set the background pts to the sum of the EO samples
-names(EObySS) <- c(group$colNm,"sampSize")
-EObySS$sampSize[EObySS[group$colNm] == "pseu-a"] <- sum(EObySS[!EObySS[group$colNm] == "pseu-a", "sampSize"])
-
-sampSizeVec <- EObySS$sampSize
-names(sampSizeVec) <- as.character(EObySS[,group$colNm])
-rm(EObySS, EObyRA)
-
-# reset sample sizes to number of points, when it is smaller than desired sample size
-# This is only relevant when complete.cases may have removed some points from an already-small set of points
-totPts <- table(df.full[,group$colNm])
-for (i in names(sampSizeVec)) if (sampSizeVec[i] > totPts[i]) sampSizeVec[i] <- totPts[i]
-rm(totPts)
-
-##
-# tune mtry ----
-#subset df to ten times pres for getting mtry  ## to increase speed
-rowCounts <- table(df.full$pres)
-if(rowCounts["0"] > (10 * rowCounts["1"])){
-  tunePres <- df.full[df.full$pres == 1, ]
-  tuneAbs <- df.full[df.full$pres == 0, ]
-  tuneAbs <- tuneAbs[sample(nrow(tuneAbs), nrow(tunePres)* 10),]
-  df.tune <- rbind(tunePres, tuneAbs)
-  rm(tuneAbs, tunePres)
-} else {
-  df.tune <- df.full
-}
-# run through mtry twice
-# very small numbers of polys can make a perfect prediction, especially
-# when subsetted like above. If error (prediction error == 0) results from 
-# subsetting, catch error and try tuning with full set
-mtry <- tryCatch(
-  {
-  x <- tuneRF(df.tune[,indVarCols],
-             y=df.tune[,depVarCol],
-             ntreeTry = 300, stepFactor = 2, mtryStart = 6,
-            strata = df.full[,group$colNm], sampsize = sampSizeVec, replace = TRUE)
-  newTry <- x[x[,2] == min(x[,2]),1]
-  y <- tuneRF(df.tune[,indVarCols],
-              y=df.tune[,depVarCol],
-              ntreeTry = 300, stepFactor = 1.5, mtryStart = max(newTry),
-              strata = df.full[,group$colNm], sampsize = sampSizeVec, replace = TRUE)
-  
-  mtry <- max(y[y[,2] == min(y[,2]),1])
-  rm(x,y, df.tune, newTry)
-  mtry
-  }, 
-  error=function(cond) {
-    message("Can't tune with subset, using full set.")
-    df.tune <- df.full
-    x <- tuneRF(df.tune[,indVarCols],
-                y=df.tune[,depVarCol],
-                ntreeTry = 300, stepFactor = 2, mtryStart = 6,
-                strata = df.full[,group$colNm], sampsize = sampSizeVec, replace = TRUE)
-    newTry <- x[x[,2] == min(x[,2]),1]
-    y <- tuneRF(df.tune[,indVarCols],
-                y=df.tune[,depVarCol],
-                ntreeTry = 300, stepFactor = 1.5, mtryStart = max(newTry),
-                strata = df.full[,group$colNm], sampsize = sampSizeVec, replace = TRUE)
-    
-    mtry <- max(y[y[,2] == min(y[,2]),1])
-    rm(x,y, df.tune, newTry)
-    mtry
-  }
-)
-rm(rowCounts)
 
 ###
 # Remove the least important env vars ----
 ##
 
-ntrees <- 1000
-numCores <- 10        
 
-# do all randomForest calls in parallel, starting with this one
-cl <- makeCluster(numCores)   
-registerDoParallel(cl)
+df.full.xgb <- xgb.DMatrix(as.matrix(df.full[,indVarCols]), 
+                           label=as.integer(as.character(df.full.x$pres)))
 
-treeSubs <- ntrees/numCores
 
-rf.find.envars <- foreach(ntree = rep(treeSubs,numCores), .combine = randomForest::combine, 
-                   .packages = 'randomForest', .multicombine = TRUE) %dopar% {
-                     randomForest(df.full[,indVarCols],
-                                  y=df.full[,depVarCol],
-                                  importance=TRUE,
-                                  ntree=ntree,
-                                  mtry=mtry,
-                                  strata = df.full[,group$colNm],
-                                  sampsize = sampSizeVec, replace = TRUE,
-                                  norm.votes = TRUE)
-                   }
+param <- list(
+  booster = "gbtree",
+  max_depth = 5, 
+  eta = 0.3, 
+  verbose = 0, 
+  nthread = 4,
+  objective = "binary:logistic", 
+  eval_metric = "auc")
 
-impvals <- importance(rf.find.envars, type = 1)
-OriginalNumberOfEnvars <- length(impvals)
+xgb.find.envars <- xgb.train(params=param, data = df.full.xgb,
+                      nrounds = 20)
+
+
+xgb.impvals <- xgb.importance(model=xgb.find.envars)
+
+#xgb already removed a bunch of env vars?
+OriginalNumberOfEnvars <- nrow(xgb.impvals)
 
 # first remove the bottom of the correlated vars
-corrdEVs <- corrdEVs[tolower(corrdEVs$gridName) %in% row.names(impvals),]
+corrdEVs <- corrdEVs[tolower(corrdEVs$gridName) %in% xgb.impvals$Feature,]
 if(nrow(corrdEVs) > 0 ){
   for(grp in unique(corrdEVs$correlatedVarGroupings)){
     vars <- tolower(corrdEVs[corrdEVs$correlatedVarGroupings == grp,"gridName"])
-    imp.sub <- impvals[rownames(impvals) %in% vars,, drop = FALSE]
-    suppressWarnings(varsToDrop <- imp.sub[!imp.sub == max(imp.sub),, drop = FALSE])
-    impvals <- impvals[!rownames(impvals) %in% rownames(varsToDrop),,drop = FALSE]
+    imp.sub <- xgb.impvals[xgb.impvals$Feature %in% vars,, drop = FALSE]
+    suppressWarnings(varsToDrop <- imp.sub[!imp.sub$Gain == max(imp.sub$Gain),, drop = FALSE])
+    xgb.impvals <- xgb.impvals[!xgb.impvals$Feature %in% varsToDrop$Feature,,drop = FALSE]
   }
   rm(vars, imp.sub, varsToDrop)
 }
@@ -313,19 +63,19 @@ if(nrow(corrdEVs) > 0 ){
 envarPctile <- 0.25
 # for gam, take top 10%
 #envarPctile <- 0.90
-y <- quantile(impvals, probs = envarPctile)
-impEnvVars <- impvals[impvals > y,]
-subsetNumberofEnvars <- length(impEnvVars)
+y <- quantile(xgb.impvals$Gain, probs = envarPctile)
+impEnvVars <- xgb.impvals[xgb.impvals$Gain > y,]
+subsetNumberofEnvars <- nrow(impEnvVars)
 rm(y)
 # which columns are these, then flip the non-envars to TRUE
-impEnvVarCols <- names(df.full) %in% names(impEnvVars)
+impEnvVarCols <- names(df.full) %in% impEnvVars$Feature
 impEnvVarCols[1:5] <- TRUE
 # subset!
 df.full <- df.full[,impEnvVarCols]
 # reset the indvarcols object
 indVarCols <- c(6:length(names(df.full)))
 
-rm(impvals, impEnvVars, impEnvVarCols)
+rm(xgb.impvals, impEnvVars, impEnvVarCols)
 
 
 
@@ -340,12 +90,6 @@ if(length(group$vals) > 50){
 
 # randomly assign group ids to background points so background pts
 # can be drawn into KFolds
-# n <- length(group$vals)
-# x <- seq(1, nrow(df.full[df.full$pres == 0,]),by=1)
-# y <- split(x, cut(x, n, labels = FALSE))
-# names(y) <- group$vals
-
-
 
 # vector to split up represents row indices
 rowInd <- seq(1, nrow(df.full[df.full$pres == 0,]),by=1)
@@ -405,8 +149,6 @@ folds <- groupKFold(df.full.s$stratum, k = kf)
 #   classProbs = TRUE
 #   )
 
-modRecipe <- recipe(pres ~ ., data = df.full.s
-                    )
 
 
 xgbfitControl <- trainControl(
@@ -433,8 +175,10 @@ df.full.s$pres <- as.factor(df.full.s$pres)
 #   min_child_weight = 1,
 #   subsample = 1
 # )
+modRecipe <- recipe(pres ~ ., data = df.full.s
+)
 
-xgbFit1 <- train(modRecipe, data = df.full.s,
+xgbFit1 <- train(pres ~ ., data = df.full.s,
                  method = "xgbTree", 
                  trControl = xgbfitControl,
                  metric = "ROC")
