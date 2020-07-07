6b5f9eadf7d844c668471dd030e16027a0b79323
Tim Howard <tghoward@gmail.com>
2020-07-07 11:15:08 -0400

    interative to iterative

--- 5_createMetadata.r
+++ 5_createMetadata.r
@@ -119,7 +119,7 @@ grank_desc <- dbGetQuery(db, SQLquery)
 NSurl <- paste("http://explorer.natureserve.org/servlet/NatureServe?searchName=",gsub(" ", "+", ElementNames[[1]], fixed=TRUE), sep="")
 
 ## get Model Evaluation and Use data ----
-SQLquery <- paste("Select spdata_dataqual, spdata_abs, spdata_eval, envvar_relevance, envvar_align, process_algo, process_sens, process_rigor, process_perform, process_review, products_mapped, products_support, products_repo, interative, spdata_dataqualNotes, spdata_absNotes, spdata_evalNotes, envvar_relevanceNotes, envvar_alignNotes, process_algoNotes, process_sensNotes, process_rigorNotes, process_performNotes, process_reviewNotes, products_mappedNotes, products_supportNotes, products_repoNotes, interativeNotes ", 
+SQLquery <- paste("Select spdata_dataqual, spdata_abs, spdata_eval, envvar_relevance, envvar_align, process_algo, process_sens, process_rigor, process_perform, process_review, products_mapped, products_support, products_repo, iterative, spdata_dataqualNotes, spdata_absNotes, spdata_evalNotes, envvar_relevanceNotes, envvar_alignNotes, process_algoNotes, process_sensNotes, process_rigorNotes, process_performNotes, process_reviewNotes, products_mappedNotes, products_supportNotes, products_repoNotes, iterativeNotes ", 
                   "FROM lkpSpeciesRubric ", 
                   "WHERE sp_code ='", model_species, "'; ", sep="")
 sdm.modeluse <- dbGetQuery(db, statement = SQLquery)
